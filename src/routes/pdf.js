d379a936398e268d03222c99d44d9f58b238299c
xander-ife <ezeasorifechukwu@gmail.com>
2026-07-06 03:50:04 +0100

    fixed auth middleware path

--- src/routes/pdf.js
+++ src/routes/pdf.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const PDFDocument = require("pdfkit");
 const { PrismaClient } = require("@prisma/client");
-const { protect } = require("../middleware/auth");
+const { protect } = require("../middleware/auth.middleware");
 
 const prisma = new PrismaClient();
 
